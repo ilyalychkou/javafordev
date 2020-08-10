eb9cb3abfbca34e5d98cdfe3960fa69b9446ef2a
Ilya Lychkou <ilya.lychkou@gmail.com>
2020-08-10 20:19:02 +0300

    lesso4 task 1

--- src/com/javafordev/lesson4/task1/Airline.java
+++ src/com/javafordev/lesson4/task1/Airline.java
@@ -1,9 +1,7 @@
 package com.javafordev.lesson4.task1;
 
-import java.util.Date;
-
 /**
- *1)Создать класс Airline:
+ * 1)Создать класс Airline:
  * Пункт назначения, +
  * Номер рейса, +
  * Тип самолета, +
@@ -12,27 +10,101 @@ import java.util.Date;
  * Создать массив объектов. Вывести:
  * a) список рейсов для заданного пункта назначения;
  * b) список рейсов для заданного дня недели;
- * c) список рейсов для заданного дня недели, время вылета для которых
- * больше заданного
- * */
+ * c) список рейсов для заданного дня недели, время вылета для которых больше заданного
+ */
 
 public class Airline {
     //поля класса
     private String destination;
     private int numberOfFlight;
     private char planeType;
-    private Date departureTime;
-    private int dayOfWeek;
+    private int departureTimeInHours;
+    private int departureTimeInMinutes;
+    private String dayOfWeek;
+
 
     //конструктор
-    public Airline(String destination, int numberOfFlight, char planeType, Date departureTime, int dayOfWeek) {
-        if (destination.equals("")) {
+    public Airline(String destination, int numberOfFlight, char planeType, int departureTimeInHours, int departureTimeInMinutes, String dayOfWeek) {
 
+        if (destination.equals("")) {
+            System.out.println("Для параметра destination задано пустое значение. "
+                    + "Допустимые значения - латиница, не пустая строка");
+            this.destination = " ";
+        } else {
+            this.destination = destination;
         }
-        this.destination = destination;
-        this.numberOfFlight = numberOfFlight;
+
+        if (numberOfFlight <= 0) {
+            System.out.println("Для параметра numberOfFlight задано отрицательное или нулевое значение. "
+                    + "Допустимые значения - целые положительные числа");
+        } else {
+            this.numberOfFlight = numberOfFlight;
+        }
+
+        if (departureTimeInHours < 0 || departureTimeInHours > 23) {
+            System.out.println("Для параметра departureTimeInHours задано недопустимое значение. "
+                    + "Допустимые значения - целые положительные числа от 0 до 23");
+        } else {
+            this.departureTimeInHours = departureTimeInHours;
+        }
+
+        if (departureTimeInMinutes < 0 || departureTimeInMinutes > 59) {
+            System.out.println("Для параметра departureTimeInMinutes задано недопустимое значение. "
+                    + "Допустимые значения - целые положительные числа от 0 до 59");
+        } else {
+            this.departureTimeInMinutes = departureTimeInMinutes;
+        }
+
         this.planeType = planeType;
-        this.departureTime = departureTime;
         this.dayOfWeek = dayOfWeek;
+    }
+
+    //метод toString()
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "destination='" + destination + '\'' +
+                ", numberOfFlight=" + numberOfFlight +
+                ", planeType=" + planeType +
+                ", departureTimeInHours=" + departureTimeInHours +
+                ", departureTimeInMinutes=" + departureTimeInMinutes +
+                ", dayOfWeek='" + dayOfWeek + '\'' +
+                '}';
+    }
+
+    //метод для выводы объектов из массива airlines по параметру destination
+    public static void printAirlinesByDestination(Airline[] airlines, String destination) {
+        int countOfResults = 0;
+        for (int i = 0; i < airlines.length; i++) {
+            if (airlines[i].destination.equals(destination)) {
+                System.out.println(airlines[i].toString());
+                countOfResults++;
+            }
+        }
+        if (countOfResults == 0) {
+            System.out.println("Не удалось наити объект массива airlines, которыи соответствует фильтру");
+        }
+    }
+
+    //метод для выводы объектов из массива airlines по параметру dayOfWeek
+    public static void printAirlinesByDayOfWeek(Airline[] airlines, String dayOfWeek) {
+        for (int i = 0; i < airlines.length; i++) {
+            if (airlines[i].dayOfWeek.equals(dayOfWeek))
+                System.out.println(airlines[i].toString());
+        }
+    }
+
+    //метод для фильтра массива airlines по параметру dayOfWeek и departureTime>заданного
+    public static void printAirlinesByDayOfWeekAndDepartureTime(Airline[] airlines, String dayOfWeek, int departureTimeInHours, int departureTimeInMinutes) {
+        int timeInMinutesToFilter = departureTimeInHours * 60 + departureTimeInMinutes;
+        for (int i = 0; i < airlines.length; i++) {
+            int timeInMinutes = airlines[i].convertHoursAndMinutesInMinutes();
+            if (airlines[i].dayOfWeek.equals(dayOfWeek) && timeInMinutes > timeInMinutesToFilter)
+                System.out.println(airlines[i].toString());
+        }
+    }
+
+    public int convertHoursAndMinutesInMinutes() {
+        return this.departureTimeInHours * 60 + this.departureTimeInMinutes;
     }
 }
--- src/com/javafordev/lesson4/task1/AirlineRunner.java
+++ src/com/javafordev/lesson4/task1/AirlineRunner.java
@@ -1,4 +1,46 @@
 package com.javafordev.lesson4.task1;
 
 public class AirlineRunner {
+/**
+ * Тестовые значения:
+ * при создании объектов
+ * 1. параметр dayOfWeek - Sun, Mon, Tue, Wed, Thu, Fri, Sat, пустое, цифра
+ * 2. параметр destination - Oslo, Ottawa, Minsk, пустое, несоответствует названию города - "ahfiewm"
+ * 3. параметр departureTimeInHours - 0, 2, 23, 24, 7, <0
+ * 4. параметр departureTimeInMinutes - 0, 7, 15, 59, 60, <0
+ * 5. параметр numberOfFlight - <0,
+ * при проверке методов фильтрации
+ *  1. параметр dayOfWeek - Sun, Mon, Tue, Wed, Thu, Fri, Sat, пустое, цифра
+ * 2. параметр destination - Oslo, Ottawa, Minsk, пустое, несоответствует названию города - "ahfiewm"
+ * 3. фильтр по departureTime - < заданного, = заданному, > заданного
+ */
+    public static void main(String[] args) {
+
+        int numberOfAirlines = 5;
+        String dayOfWeekToFilter = "Fri";
+        String destinationToFilter = "Oslo";
+        int departureTimeInHoursToFilter = 5;
+        int departureTimeInMinutesToFilter = 22;
+        //инициализация массива объектов типа Airline
+        Airline[] airlines = new Airline[numberOfAirlines];
+
+        airlines[0] = new Airline("Oslo", 0, 'A', 22, 10, "Fri");
+        airlines[1] = new Airline("Warsaw", 1540, 'B', 9, 3, "Mon");
+        airlines[2] = new Airline("Ottawa", 1004, 'A', 7, 37, "Tue");
+        airlines[3] = new Airline("Tokyo", 936, 'C', 16, 21, "Wed");
+        airlines[4] = new Airline("Minsk", 2255, 'B', 4, 21, "Fri");
+
+        //вывод списка объектов из массива airlines для заданного destinationToFilter
+        Airline.printAirlinesByDestination(airlines, destinationToFilter);
+        System.out.println();
+
+        //вывод списка объектов из массива airlines для заданного dayOfWeekToFilter
+        Airline.printAirlinesByDayOfWeek(airlines, dayOfWeekToFilter);
+        System.out.println();
+
+        //вывод списка объектов из массива airlines для заданного dayOfWeekToFilter и departureTime > заданного
+        Airline.printAirlinesByDayOfWeekAndDepartureTime(airlines, dayOfWeekToFilter, departureTimeInHoursToFilter, departureTimeInMinutesToFilter);
+        System.out.println();
+
+    }
 }
