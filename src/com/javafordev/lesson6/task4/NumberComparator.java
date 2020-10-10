aff167e2f98f0d214b492d05ee97bc53992c66f1
Ilya Lychkou <ilya.lychkou@gmail.com>
2020-10-10 09:51:27 +0300

    lesson6 task4 NumberComparator fixed

--- src/com/javafordev/lesson6/task4/NumberComparator.java
+++ src/com/javafordev/lesson6/task4/NumberComparator.java
@@ -6,16 +6,6 @@ public class NumberComparator implements Comparator<Integer> {
 
     @Override
     public int compare(Integer o1, Integer o2) {
-        NumberValidator.validateInputNumber(o1);
-        NumberValidator.validateInputNumber(o2);
-        int returnValue = 0;
-        if (o1.intValue() > 0 && o2.intValue() < 0) {
-            returnValue = -1;
-        } else if (o1.intValue() == 0 || o2.intValue() == 0) {
-            returnValue = -1;
-        } else if (o1.intValue() < 0 && o2.intValue() > 0) {
-            returnValue = 1;
-        }
-        return returnValue;
+        return o2.compareTo(o1);
     }
 }
