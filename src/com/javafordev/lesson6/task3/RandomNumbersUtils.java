eeee9a81c91aac3614134f66eff3142ebd4081a5
Ilya Lychkou <ilya.lychkou@gmail.com>
2020-10-10 09:46:39 +0300

    lesson6 task3 randoNumber algorithm fixed

--- src/com/javafordev/lesson6/task3/RandomNumbersUtils.java
+++ src/com/javafordev/lesson6/task3/RandomNumbersUtils.java
@@ -1,8 +1,17 @@
 package com.javafordev.lesson6.task3;
 
+import java.util.Random;
+
 public class RandomNumbersUtils {
 
     public static int generateRandomNumber() {
-        return (int)(Math.random() * 10);
+
+        int min = 124;
+        int max = 463;
+        int diff = max - min;
+        Random random = new Random();
+        int randomNumber = random.nextInt(diff+1);
+        randomNumber +=min;
+        return randomNumber;
     }
 }
