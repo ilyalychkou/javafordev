218c568695de6b9c34607dae48a6397810c2614f
Ilya Lychkou <ilya.lychkou@gmail.com>
2020-10-10 08:05:29 +0300

    lesson6 task1 randoGrade algorithm fixed

--- src/com/javafordev/lesson6/task1/GradeUtils.java
+++ src/com/javafordev/lesson6/task1/GradeUtils.java
@@ -1,12 +1,16 @@
 package com.javafordev.lesson6.task1;
 
+import java.util.Random;
+
 public class GradeUtils {
 
     public static int generateRandomGrade() {
-        int randomGrade = (int) (Math.random() * 10);
-        if (randomGrade == 0) {
-            randomGrade = 1;
-        }
+        int min = 12;
+        int max = 45;
+        int diff = max - min;
+        Random random = new Random();
+        int randomGrade = random.nextInt(diff+1);
+        randomGrade +=min;
         return randomGrade;
     }
 }
